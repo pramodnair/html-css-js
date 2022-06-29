334d4aa2b794e9170df78f8dbdea1f3cd98c5881
Pramod Nair <pramod.nair@protegrity.com>
2022-06-29 09:09:13 +0530

    add expanding functionality

--- expanding-cards/scripts/script.js
+++ expanding-cards/scripts/script.js
@@ -1,1 +1,22 @@
 // All the javascript code goes here.
+const cards = document.querySelectorAll(".panel");
+
+function activateCard() {
+  cards.forEach((card) => {
+    // set action on click event
+    card.addEventListener("click", () => {
+      // de-activate previous cards
+      deactivateCards();
+      // activate the current card
+      card.classList.add("active");
+    });
+  });
+}
+
+function deactivateCards() {
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+}
+
+activateCard();
